3a40fdf04c7560c2cd6d5ec11efd4f9334d0f2a4
onishi-teppei <teppei99.nxw@gmail.com>
2025-03-16 18:56:41 +0900

    vpc作成

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+// import { CdkStack } from '../lib/cdk-stack';
+import {VpcStack} from '../lib/vpc-stack';
 
 const app = new cdk.App();
 
@@ -11,18 +12,26 @@ const envKey = app.node.tryGetContext(argContext);
 const envVals = app.node.tryGetContext(envKey);
     if (envVals == undefined) throw new Error('Invalid environment.');
 
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// 環境変数を設定
+const env = { account: envVals['env']['account'], region: envVals['env']['region'] };
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// VPC Stackを作成
+const vpcStack = new VpcStack(app, 'VpcStack', {
+  env,
 });
+
+// new CdkStack(app, 'CdkStack', {
+//   /* If you don't specify 'env', this stack will be environment-agnostic.
+//    * Account/Region-dependent features and context lookups will not work,
+//    * but a single synthesized template can be deployed anywhere. */
+
+//   /* Uncomment the next line to specialize this stack for the AWS Account
+//    * and Region that are implied by the current CLI configuration. */
+//   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+//   /* Uncomment the next line if you know exactly what Account and Region you
+//    * want to deploy the stack to. */
+//   // env: { account: '123456789012', region: 'us-east-1' },
+
+//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// });

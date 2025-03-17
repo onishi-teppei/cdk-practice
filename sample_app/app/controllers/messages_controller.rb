d89b7fd5d6d5f61528340b0b8ea1d72054803130
onishi-teppei <teppei99.nxw@gmail.com>
2025-03-17 17:32:16 +0900

    view作成

--- sample_app/app/controllers/messages_controller.rb
+++ sample_app/app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      @messages = Message.all
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end

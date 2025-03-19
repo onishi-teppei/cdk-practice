de88170afe1c9cfbff39995e656093b85c9f6cbb
onishi-teppei <teppei99.nxw@gmail.com>
2025-03-20 00:44:10 +0900

    amdに変更

--- sample_app/config/environments/production.rb
+++ sample_app/config/environments/production.rb
@@ -103,4 +103,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << "to2go_test.nexlink2.jp"
+  config.hosts.clear
 end

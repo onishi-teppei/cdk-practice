3eff58876ed12097dbb0d196a135e5713a31a79f
onishi-teppei <teppei99.nxw@gmail.com>
2025-03-20 01:55:50 +0900

    assets:precompile対応

--- sample_app/config/environments/production.rb
+++ sample_app/config/environments/production.rb
@@ -104,4 +104,6 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << "to2go_test.nexlink2.jp"
   config.hosts.clear
+  config.assets.digest = true
+  config.assets.precompile += %w(application.js application.css)
 end
